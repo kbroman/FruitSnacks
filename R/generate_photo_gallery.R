939ec172de40a44e2d890e80ce9c85791928e659
Karl Broman <kbroman@gmail.com>
2015-02-07 00:02:11 -0600

    Put a photo gallery in the root directory, too.

--- R/generate_photo_gallery.R
+++ R/generate_photo_gallery.R
@@ -1,3 +1,5 @@
 fig_files <- list.files("../Photos", pattern = "fruitsnacks.*\\.png$")
 writeLines(paste0(fig_files, "\n",
                   "![`", fig_files, "`](", fig_files, ")\n\n"), "../Photos/ReadMe.md")
+writeLines(paste0("Photos/", fig_files, "\n",
+                  "![`Photos/", fig_files, "`](Photos/", fig_files, ")\n\n"), "../PhotoGallery.md")
